ef02248ef860f55aeeb4db4e26c6e322efda1ce4
antlad <antlad@wks700.elektron>
2015-02-03 09:48:38 +0400

    starting oci

---
+++ tests/ocidataset.cpp
@@ -0,0 +1,12 @@
+#include "ocidataset.h"
+
+OCIDataSet::OCIDataSet()
+{
+
+}
+
+OCIDataSet::~OCIDataSet()
+{
+
+}
+
---
+++ tests/ocidataset.h
@@ -0,0 +1,12 @@
+#ifndef OCIDATASET_H
+#define OCIDATASET_H
+
+
+class OCIDataSet
+{
+public:
+	OCIDataSet(const std::string& file);
+	~OCIDataSet();
+};
+
+#endif // OCIDATASET_H
